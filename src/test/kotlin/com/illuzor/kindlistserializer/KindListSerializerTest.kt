9fbb998d59f32284d5ed7bbe6d9ab33cc0a14166
Artem Zanin <illuzor@gmail.com>
2021-10-27 18:59:27 +0300

    ThrowableHandler invocations test

--- src/test/kotlin/com/illuzor/kindlistserializer/KindListSerializerTest.kt
+++ src/test/kotlin/com/illuzor/kindlistserializer/KindListSerializerTest.kt
@@ -1,14 +1,18 @@
 package com.illuzor.kindlistserializer
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.*
 
 class KindListSerializerTest {
 
     @Serializable
     private data class Data(val name: String, val value: Int)
+
+    private val mockThrowableHandler: ThrowableHandler = mock()
 
     private val testData =
         listOf(
@@ -23,33 +27,41 @@ class KindListSerializerTest {
     private val testBrokenString =
         """[{"name":"Name1","value":11},{"name":[]},"vvv",["a", "b", "v"],{"name":"Name3"},{"name":"Name2","value":22},{"name":"Name3","value":33},{"name":"Name3"}]"""
 
-    @Test
-    fun `Decode with broken string - success`() {
-        val serializer = kindListSerializer(Data.serializer())
-
-        assertEquals(
-            testData,
-            Json.decodeFromString(serializer, testBrokenString)
-        )
-    }
 
     @Test
     fun `Decode with correct string - success`() {
-        val serializer = kindListSerializer(Data.serializer())
+        val serializer = getSerializer()
 
         assertEquals(
             testData,
             Json.decodeFromString(serializer, testCorrectString)
         )
+
+        verify(mockThrowableHandler, never()).handle(any())
+    }
+
+    @Test
+    fun `Decode with broken string - success`() {
+        val serializer = getSerializer()
+
+        assertEquals(
+            testData,
+            Json.decodeFromString(serializer, testBrokenString)
+        )
+
+        verify(mockThrowableHandler, times(5)).handle(any())
     }
 
     @Test
     fun `Encode to string - success`() {
-        val serializer = kindListSerializer(Data.serializer())
+        val serializer = getSerializer()
 
         assertEquals(
             testCorrectString,
             Json.encodeToString(serializer, testData)
         )
     }
+
+    private fun getSerializer(): KSerializer<List<Data>> =
+        kindListSerializer(Data.serializer(), mockThrowableHandler)
 }
